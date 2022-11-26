c3065e099e8e4fddb5fa97a00a575f41305b9fc3
Beanca Guerragit config -- global user.name <beancaguerra>
2022-11-26 22:20:34 +0800

    fixed error

--- move_to_archive.php
+++ move_to_archive.php
@@ -2,15 +2,19 @@
 
 include 'connect_db.php';
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if(!isset($_SESSION['admin_id'])){
+    echo "Go back";
+    header("Location: admin_login.php");
 }
 
 $id = $_GET['id'];
 $recipient_id = $_GET['acctId'];
 $itemNo = $_GET['itemNo'];
 $xadmin_id =$_GET['admin_id'];
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 
 $textmessage = "Dear Mr/Ms. <br> We want to inform you that the details/image you sent to us does not match to the item that we had.";
