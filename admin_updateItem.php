138a3cbd930944b4a288b6932c04664dcfcad645
Beanca Guerragit config -- global user.name <beancaguerra>
2022-11-26 17:07:32 +0800

    fixing code

--- admin_updateItem.php
+++ admin_updateItem.php
@@ -103,6 +103,11 @@
                                     <p class="p-one"><span style='color:#ec9006; font-weight:700; margin-right: 20px;'>Item Location: </span><?php echo $row['itemLocation']; ?></p>
                                     
                                 </div>
+                                <div class="output-three output">
+                                    <p class="p-two"><span style='color:#ec9006; font-weight:700; margin-right: 20px;'>Item Brand: </span><?php echo $row['itemBrand']; ?></p>
+                                    <p class="p-one"><span style='color:#ec9006; font-weight:700; margin-right: 20px;'>Item Color: </span><?php echo $row['itemColor']; ?></p>
+                                    
+                                </div>
                                 <div class="output-one output">
                                     <p class="p-two"><span style='color:#ec9006; font-weight:700; margin-right: 20px;'>Description: </span><?php echo $row['itemDescription']; ?></p>
                                     <p class="p-one" readonly><span style='color:#ec9006; font-weight:700; margin-right: 20px;'>Item No: </span><?php echo $row['itemNo']; ?></p>
