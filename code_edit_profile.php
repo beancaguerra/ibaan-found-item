a20542d98ecf9c609c488626494ad5f128726e03
Beanca Guerragit config -- global user.name <beancaguerra>
2022-11-26 13:40:22 +0800

    fixing error

--- code_edit_profile.php
+++ code_edit_profile.php
@@ -73,9 +73,10 @@
                                 padding: 0 5px;">
 
                         <?php
+                        header("Access-Control-Allow-Origin: *");
 
                         ini_set('display_errors', 1);
-                        error_reporting(E_ALL & ~E_NOTICE);
+                        //error_reporting(E_ALL & ~E_NOTICE);
                         Error_reporting(0);
 
                         include 'connect_db.php';
