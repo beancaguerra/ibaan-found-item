c37cc182d4f576cf5aec269e0c3a1d6a29ca43e0
Beanca Guerragit config -- global user.name <beancaguerra>
2022-11-27 18:29:05 +0800

    change code

--- admin_reports.php
+++ admin_reports.php
@@ -41,97 +41,97 @@
                 <div class="profile-cont">
                     <div class="container">
                         <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card mt-5">
-                    <div class="card-header">
-                        <h4>Reports of Items</h4>
-                    </div>
-                    <div class="card-body">
-                    
-                        <form action="" method="GET">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>From Date</label>
-                                        <input type="date" name="from_date" value="<?php if(isset($_GET['from_date'])){ echo $_GET['from_date']; } ?>" class="form-control">
+                            <div class="col-md-12">
+                                <div class="card mt-5">
+                                    <div class="card-header">
+                                        <h4>Reports of Items</h4>
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>To Date</label>
-                                        <input type="date" name="to_date" value="<?php if(isset($_GET['to_date'])){ echo $_GET['to_date']; } ?>" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label>Click to Filter</label> <br>
-                                      <button type="submit" class="btn btn-primary">Filter</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div class="card mt-4">
-                    <div class="card-body">
-                        <table class="table table-borderd">
-                            <thead>
-                                <tr>
-                                    <th>Item No</th>
-                                    <th>Item Category</th>
-                                    <th>Item Location</th>
-                                    <th>Item Brand</th>
-                                    <th>Item Color</th>
-                                    <th>Item Description</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            
-                            <?php 
-                                include 'connect_db.php';
-                                
-                                ini_set('display_errors',1);
-                                //error_reporting(E_ALL & ~E_NOTICE);
-                                Error_reporting(0);
-
-                                if(isset($_GET['from_date']) && isset($_GET['to_date']))
-                                {
-                                    $from_date = $_GET['from_date'];
-                                    $to_date = $_GET['to_date'];
-
-                                    $query="SELECT * FROM tb_itemrecord WHERE date BETWEEN '$from_date' AND '$to_date' ";
-                                    $query_run = mysqli_query($con, $query);
+                                    <div class="card-body">
                                     
-                                    if(mysqli_num_rows($query_run) > 0)
-                                    {
-                                        foreach($query_run as $row)
-                                        {
-                                            ?>
-                                            <tr>
-                                                <td><?= $row['itemNo']; ?></td>
-                                                <td><?= $row['itemCategory']; ?></td>
-                                                <td><?= $row['itemLocation']; ?></td>
-                                                <td><?= $row['itemBrand']; ?></td>
-                                                <td><?= $row['itemColor']; ?></td>
-                                                <td><?= $row['itemDescription']; ?></td>
-                                            </tr>
-                                            <?php
-                                        }
-                                    }
-                                    else
-                                    {
-                                        echo "No Record Found";
-                                    }
-                                }
-                            ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                                        <form action="" method="GET">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label>From Date</label>
+                                                        <input type="date" name="from_date" value="<?php if(isset($_GET['from_date'])){ echo $_GET['from_date']; } ?>" class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label>To Date</label>
+                                                        <input type="date" name="to_date" value="<?php if(isset($_GET['to_date'])){ echo $_GET['to_date']; } ?>" class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label>Click to Filter</label> <br>
+                                                        <button type="submit" class="btn btn-primary">Filter</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
 
-            </div>
-        </div>
+                                <div class="card mt-4">
+                                    <div class="card-body">
+                                        <table class="table table-borderd">
+                                            <thead>
+                                                <tr>
+                                                    <th>Item No</th>
+                                                    <th>Item Category</th>
+                                                    <th>Item Location</th>
+                                                    <th>Item Brand</th>
+                                                    <th>Item Color</th>
+                                                    <th>Item Description</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            
+                                            <?php 
+                                                include 'connect_db.php';
+                                                
+                                                ini_set('display_errors',1);
+                                                //error_reporting(E_ALL & ~E_NOTICE);
+                                                Error_reporting(0);
+
+                                                if(isset($_GET['from_date']) && isset($_GET['to_date']))
+                                                {
+                                                    $from_date = $_GET['from_date'];
+                                                    $to_date = $_GET['to_date'];
+
+                                                    $query="SELECT * FROM tb_itemrecord WHERE date BETWEEN '$from_date' AND '$to_date' ";
+                                                    $query_run = mysqli_query($con, $query);
+                                                    
+                                                    if(mysqli_num_rows($query_run) > 0)
+                                                    {
+                                                        foreach($query_run as $row)
+                                                        {
+                                                            ?>
+                                                            <tr>
+                                                                <td><?= $row['itemNo']; ?></td>
+                                                                <td><?= $row['itemCategory']; ?></td>
+                                                                <td><?= $row['itemLocation']; ?></td>
+                                                                <td><?= $row['itemBrand']; ?></td>
+                                                                <td><?= $row['itemColor']; ?></td>
+                                                                <td><?= $row['itemDescription']; ?></td>
+                                                            </tr>
+                                                            <?php
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        echo "No Record Found";
+                                                    }
+                                                }
+                                            ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
