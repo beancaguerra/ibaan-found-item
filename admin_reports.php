0c61e7f2b32011b4d692f7e16e908867fed99ae9
Beanca Guerragit config -- global user.name <beancaguerra>
2022-11-28 01:45:29 +0800

    ok

--- admin_reports.php
+++ admin_reports.php
@@ -49,8 +49,8 @@
                                         <input class="input big" type="date" placeholder="Start Date" name="date1" value="<?php echo isset($_POST['date1']) ? $_POST['date1'] : '' ?>" />
                                         <input class="input small" type="date" placeholder="End Date" name="date2" value="<?php echo isset($_POST['date2']) ? $_POST['date2'] : '' ?>"/>
                                         <select name="claim">
-                                            <option value="0">CLAIMED</option>
-                                            <option value="1">NOT CLAIMED</option>
+                                            <option value="1">CLAIMED</option>
+                                            <option value="0">NOT CLAIMED</option>
                                         </select>
                                     </div>
                                     <div class="first-three">
