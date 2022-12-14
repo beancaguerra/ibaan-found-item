830f7469d3ae41fc13011909a4477a8756c88837
GuerraMarielZ <ldmvbg@gmail.com>
2022-12-14 22:05:50 +0800

    change log in design

--- admin_login.php
+++ admin_login.php
@@ -1,97 +1,148 @@
+<?php include_once ("controller.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Lost-Item Finder System in Ibaan</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="icon" href="./images/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">    
-    </script>
-    <link rel="stylesheet" href="page.css">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Online Lost-Item Finder System in Ibaan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon"> -->
+  <link rel="icon" href="./images/logo.png" type="image/x-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="index.css" rel="stylesheet">
+  <link href="assets/css/page.css" rel="stylesheet">
+
 </head>
+
 <body>
-    <!-- Start header -->
-    <div class="container" id="container" style="width: 100%;">
-        <div class="header" id="header">
-            <img src="./images/ibaan.png" width="100%" height= "55%";/>
-        </div>
+  
+  <header id="header" class="header d-flex align-items-center">
 
-        <div class="topnav">
-            <p>ADMINISTRATOR</p>
-        </div>
-    <!-- End Header -->
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Lost Item Finder<span>.</span></h1>
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#signin">Sign in</a></li>
+        </ul>
+      </nav><!-- .navbar -->
 
-    <div class="offer_section layout_padding-bottom">
-        <div class="offer_container">
-            <div class="container" style="padding: 30px 20% 30px 20%;">
-                <!-- log in form -->
-                <div class="col-md justify-content-center">
-                    <div class="box">
-                        <div class="detail-box vh-40" style = "margin: 10px 20px;">
-                            <div class="vh-100 gradient-custom">
-                                <div class="container py-12 h-100">
-                                    <div class="row d-flex justify-content-center align-items-center h-100">
-                                        <div class="col-10 col-md-10 col-lg-10 col-xl-10">
-                                            <div class="card bg-light text-black">
-                                                <div class="card-body p-4">
-                                                    <div class="mb-md-4 mt-md-4 pb-4">
-                                                        <h2 class="fw-bold mb-4 text-center">Log in</h2>
-                                                        <hr class="mb-2 ">
-                                                        <form action="admin_signin.php" method="POST">
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
 
-                                                            <?php
-                                                                $remarks = isset($_GET['remark_login']) ? $_GET['remark_login'] : '';
-                                                                if ($remarks=='failed') {
-                                                                    echo ' <div style="height: auto;
-                                                                                width: 100%;
-                                                                                background: #ec9006;
-                                                                                padding: 0 15px;
-                                                                                font-size: 19px;
-                                                                                line-height: 40px;
-                                                                                margin: 10px 0;
-                                                                                color: #000;
-                                                                                border-radius: 4px;"> Incorrect email or password</div> ';
-                                                                }
-                                                            ?>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero" style="height: 550px;">
+    <img src="./assets/img/ibaan.jpg" style="width: 100%;
+                                             position: absolute;
+                                             top: 0px;">
+  </section>
+  <!-- End Hero Section -->
 
-                                                            <div class="form-outline form-white mb-2">
-                                                                <label for="email">Email</label>
-                                                                <input type="email" id="email" class="form-control form-control-lg" name="admin_email" placeholder="Enter your email..." required/>
-                                                                
-                                                            </div>
+  <!-- Sign in Section -->
+  <section id="signin">
+      <div class="container position-relative">
+      <div class="row gy-5">
+        <div class="col-lg-4 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start" 
+                style="margin-right: 15%;
+                        margin-top: 0px;
+                        border-radius: 5px;">
+            <div class="text-white" style="background-color: rgba(0, 0, 0, 0.592); padding-left: 10px; height: 75%; padding-top: 15px;">
+                <div class="card-body p-12">
+                    <div class="mb-md-12 mt-md-12 pb-12">
+                        <h4 style="text-align: center;">Please log in to your account!</h4>
+                        <div class="d-flex justify-content-center justify-content-lg-start">
+                            <form action="admin_signin.php" method="POST">
+                                <?php
+                                    $remarks = isset($_GET['remark_login']) ? $_GET['remark_login'] : '';
+                                    if ($remarks=='failed') {
+                                        echo ' <div style="height: auto;
+                                                    width: 100%;
+                                                    background: #ec9006;
+                                                    padding: 0 15px;
+                                                    font-size: 19px;
+                                                    line-height: 40px;
+                                                    margin: 10px 0;
+                                                    color: #000;
+                                                    border-radius: 4px;"> Incorrect email or password</div> ';
+                                    }
+                                ?>
 
-                                                            <div class="form-outline form-white mb-4">
-                                                                <label for="password">Password</label>
-                                                                <input type="password" id="password" class="form-control form-control-lg" name="admin_password" placeholder="Enter your password..." required/>
-                                                                
-                                                                <input style="margin-top: 22px; margin-bottom:25px; margin-right: 12px;" type="checkbox" onclick="myFunction()">Show Password
-                                                            </div>
-
-                                                            <button class="sign-btn" type="submit">Log in</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div class="form-outline form-white" style="width: 328px;">
+                                    <label for="email">Email</label>
+                                    <input type="email" id="typeEmailX" class="form-control form-control-sm" id="email" name="admin_email" placeholder="Enter your email..." required />
+                                    
                                 </div>
-                            </div>
+
+                                <div class="form-outline form-white" style="width: 328px;">
+                                    <label for="password">Password</label>
+                                    <input type="password" id="pass" class="form-control form-control-sm" name="admin_password" placeholder="Enter your password..." required/>
+                                    
+                                </div>
+
+                                <input style="margin-top: 3px; margin-bottom:25px; margin-right: 12px;" type="checkbox" onclick="myFunction()">Show Password
+
+                                <p class="small mb-4 pb-lg-2 text-center" ><a class="text-white-500" style="color: white;" href="forgot.php">Forgot password?</a></p>
+
+                                <button class="sign-btn" type="submit" name="login">Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-lg-6 order-1 order-lg-2">
+          <img src="./images/found.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100" style="width: 80%; height: 80%; margin-top: 8%;">
+        </div>
+      </div>
     </div>
-</body>
+  </section>
+  <!-- End of sign in section -->
 
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+</body>
 <script>
-     // these code are for show password
+
+    // these code are for show password
     function myFunction() {
-      var x = document.getElementById("password");
+      var x = document.getElementById("pass");
       
       if (x.type === "password") {
         x.type = "text";
