6518e5d295c20df58bd6ef46627c1f456e9449d4
GuerraMarielZ <ldmvbg@gmail.com>
2022-12-18 18:24:22 +0800

    change images

--- index.php
+++ index.php
@@ -59,10 +59,11 @@
   <!-- End Header -->
 
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="hero" style="height: 550px;">
-    <img src="./assets/img/kultura.jpg" style="width: 100%;
+  <section id="hero" class="hero" style="height: 880px;">
+    <img src="./images/kultura.jpg" style="width: 100%;
                                              position: absolute;
-                                             top: 0px;">
+                                             top: 0px;
+                                             height: auto;">
   </section>
   <!-- End Hero Section -->
 
