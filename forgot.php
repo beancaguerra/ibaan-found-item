bd00557e8fbbf54b0c10bcf63beada92ea0fbe2b
GuerraMarielZ <ldmvbg@gmail.com>
2023-01-04 11:39:56 +0800

    fixed css

--- forgot.php
+++ forgot.php
@@ -97,35 +97,8 @@
 </div>
 </body>
 <style>
-    @media screen and (min-width: 400px) and (max-width: 465px){
-        .detail-box{
-            width: 432px;
-            margin-left: -138px;
-        }
-        #alert {
-            font-size: 12px;
-        }
-    }
-    @media screen and (min-width: 465px) and (max-width: 667px){
-        .detail-box{
-            width: 432px;
-            margin-left: -138px;
-        }
-        #alert {
-            font-size: 12px;
-        }
-    }
-    @media screen and (min-width: 667px) and (max-width: 867px){
-        .detail-box{
-            width: 432px;
-            margin-left: -138px;
-        }
-        #alert {
-            font-size: 12px;
-        }
-    }
     @media screen and (max-width: 900px){
-        .container .box{
+        .detail-.box{
             width: 334px;
             margin-left: -93px;
         }
