5e906aa135ada0dd6c2d653dde4e7cd0ab45d1a6
GuerraMarielZ <ldmvbg@gmail.com>
2023-03-04 13:29:32 +0800

    fixed code

--- viewer_messages.php
+++ viewer_messages.php
@@ -262,10 +262,34 @@
         }
     </style>
     <script type="text/javascript">
+        //Get the button
+        var mybutton = document.getElementById("myBtn");
+            
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
         
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+        }
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        }
+        //small devices navigation
+        function myNav() {
+            document.getElementById("navigation").style.display = "block";
+            }
+        function closeNav() {
+            document.getElementById("navigation").style.display = "none";
+            }
         
 
-
+        //code in messages
         function getMsg(xID,xfullName){
             $("#fullNameHead").text(xfullName);
             getChat(xID);
